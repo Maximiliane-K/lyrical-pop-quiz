b313c925d79a2c3d9373e1faadde70b7cffca9fa
Maximiliane-K <>
2023-05-05 21:42:09 +0000

    Add event listener for loading of the DOM and hide elemnts of sections which aren't the landingpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-function hideIntroSection() {
-    let show = document.getElementsByClassName("intro");
-    if (show.style.display === "none") {
-        show.style.display = "block";
-      } else {
-        show.style.display = "none";
-      }
+//When the DOM is finished loading all sections except the intro-section shall be hidden
+// Code used for hiding elements https://www.geeksforgeeks.org/hide-or-show-elements-in-html-using-display-property/
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    //Hide form section for username
+    document.querySelector(".user").style.display = "none";
+
+    //Hide main game section 
+    document.querySelector(".gamePlay").style.display = "none";
+
+    //Hide result page
+    document.querySelector(".result").style.display = "none";
+});
+
+
+/**
+ * Hides the two buttons on the intro page when "Start Game" is clicked 
+ */
+
+let intro = document.querySelector(".intro");
+function showHideIntro() {
+    intro.style.display = "none";
 }
